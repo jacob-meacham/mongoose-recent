e22b0794bee81bfeee50e556dc84c7eb56683570
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-04 20:29:39 -0700

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/*
+ * grunt-contrib-jshint
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2015 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+        files: ['Gruntfile.js', 'index.js', 'test/**/*.js'],
+        options: {
+            jshintrc: true
+        }
+    },
+    
+    mocha_istanbul: {
+      coverage: {
+          src: 'test/**/*.spec.js',
+          options: {
+              coverageFolder: 'build/coverage',
+              reportFormats: ['lcov'],
+          }
+      }
+    },
+
+    lcovMerge: {
+      options: {
+          emitters: ['file', 'event'],
+          outputFile: 'out.lcov'
+      },
+      files: ['build/coverage/*.info', 'build/coverage/**/*.info']
+    }
+  });
+
+  grunt.event.on('coverage', function(lcov, done) {
+    // Coveralls uses argv to set the basePath
+    var oldArgv = process.argv[2];
+    process.argv[2] = '';
+    require('coveralls').handleInput(lcov, function(err) {
+      process.argv[2] = oldArgv;
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('ci', ['jshint', 'mocha_istanbul', 'lcovMerge']);
+  grunt.registerTask('default', ['jshint', 'mocha_istanbul']);
+
+};
