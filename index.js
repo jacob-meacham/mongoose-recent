c1e474bf2304b16796677f65fc60d473a66b00bd
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-04 21:59:06 -0700

    A bit of cleanup, add an option for allowing dupes.

--- index.js
+++ index.js
@@ -3,32 +3,19 @@
 var _ = require('lodash');
 var DefaultObjectId = require('mongoose').Schema.Types.ObjectId;
 
-// Mongoose recently-viewed plugin
-//     - number of recently-viewed to keep
-//     - are duplicates allowed
-//     - set the name of the key that has recently viewed
-//     - set the id type or object in the thing
-//         - if possible, have a options.idType = whatever, but also a options.subdocument that lets you explicitly set a subdocument
-//     - addRecent* where * is set based on the name of the key (or set explicitly)
-//         - eg options.name = 'flows' then we would recentFlows as the key, and the function addRecentFlow. If you want to have more control, use options.collectionName='recentlyViewedFlows' and options.functionName='addANewRecentFlow'
-//         addRecentFlow(documentOrId);
-
 // TODO: Promise or callback
 var setupOptions = function(options) {
   options = options || {};
 
   var defaults = {
-    idType: DefaultObjectId,
+    name: 'view',
     numToKeep: 10,
-    name: 'views',
-    dateName: 'date'
+    idType: DefaultObjectId,
+    dateName: 'date',
+    duplicatesAllowed: false
   };
 
-  // TODO: Merge via lodash
-  options.idType = options.idType || defaults.idType;
-  options.numToKeep = options.numToKeep || defaults.numToKeep;
-  options.name = options.name || defaults.name;
-  options.dateName = options.dateName || defaults.dateName;
+  _.extend(options, defaults);
 
   if (!options.compareFunc) {
     options.compareFunc = function(o1, o2) {
@@ -41,19 +28,20 @@ var setupOptions = function(options) {
 
 var generateAddFunction = function(collectionName, options) {
   return function(objectOrId, cb) {
-    // TODO: This won't work for a static.
-    var foundIdx = _.findIndex(this[collectionName], options.compareFunc);
-    if (foundIdx !== -1) {
-      var entry = this[collectionName][foundIdx];
-      entry[options.dateName] = Date.now();
-    } else {
-      // TODO: Not atomic, not sure if $addToSet is atomic either
+    // TODO: Doesn't work in static case - need to do a find and update instead.
+    var collection = this[collectionName];
+    
+    var foundIdx = _.findIndex(collection, options.compareFunc);
+    if (options.duplicatesAllowed || foundIdx === -1) {
       var newObject = {};
       newObject[options.name] = objectOrId;
-      this[collectionName].push(newObject);
+      collection.push(newObject);
+    } else {
+      var entry = collection[foundIdx];
+      entry[options.dateName] = Date.now();
     }
 
-    this[collectionName] = _(this[collectionName]).sortBy('date').reverse().slice(0, options.numToKeep).value();
+    this[collectionName] = _(collection).sortBy('date').reverse().slice(0, options.numToKeep).value();
     this.save(cb);
   };
 };
@@ -61,9 +49,9 @@ var generateAddFunction = function(collectionName, options) {
 var plugin = function(schema, options) {
   options = setupOptions(options);
   
-  // TODO: Uppercase name
-  var collectionName = options.collectionName || 'recent' + options.name;
-  var addFuncName = options.addFunctionName || 'addRecent' + options.name.slice(0, -1);
+  var upperCasedName = options.name.charAt(0).toUpperCase() + options.name.slice(1);
+  var collectionName = options.collectionName || 'recent' + upperCasedName + 's';
+  var addFuncName = options.addFunctionName || 'addRecent' + upperCasedName;
 
   var schemaAdditions = {};
   var docSchema = {};
